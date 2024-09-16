b69af7d52573675e3c5cc1c7ae2f9137924d14a7
Giulio Mazzanti <6185775+Giuzzilla@users.noreply.github.com>
2024-09-16 22:47:01 +0200

    feat: blog and first article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import { rehypeShiki } from '@astrojs/markdown-remark'
+import rehypeMermaid from 'rehype-mermaid'
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind()],
+  site: "https://giuzzilla.dev",
+  markdown: {
+    rehypePlugins: [
+      rehypeMermaid,
+      rehypeShiki,
+    ],
+    syntaxHighlight: false,
+  }
 });
